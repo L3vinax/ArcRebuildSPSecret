c08816b77334cad67918636d8f38f93cac9c7123
Aaron Hutchinson <111082932+L3vinax@users.noreply.github.com>
2024-04-04 07:07:33 -0700

    Update ArcUpdateSP.ps1

--- ArcUpdateSP.ps1
+++ ArcUpdateSP.ps1
@@ -1,19 +1,22 @@
-Import-module ActiveDirectory
+Import-Module AzureArcDeployment.psm1
 
-[Parameter(Mandatory = $True)]
-[System.String]$ServicePrincipalSecret
+$DomainFQDN = "DOMAIN.FQDN"
+$ServicePrincipalSecret = "ServicePrincipalPlainText"
+$SecretFile = "PATH_TO_ARC_DEPLOY\encryptedServicePrincipalSecret"
 
-#Add Access to Domain Computers and Domain Controllers
+$DomainNetbios = (Get-ADDomain $DomainFQDN).NetBIOSName
 $DomainComputersSID = (Get-ADDomain).DomainSID.Value + '-515'
 $DomainComputersName = (Get-ADGroup -Filter "SID -eq `'$DomainComputersSID`'").Name
 $DomainControllersSID = (Get-ADDomain).DomainSID.Value + '-516'
 $DomainControllersName = (Get-ADGroup -Filter "SID -eq `'$DomainControllersSID`'").Name
 
 
-# Encrypting the ServicePrincipalSecret to be decrypted only by the Domain Controllers and the Domain Computers security groups
+$identity = "$DomainNetbios\$DomainComputersName"
+$identity2 = "$DomainNetbios\$DomainControllersName"
+
 $DomainComputersSID = "SID=" + $DomainComputersSID
 $DomainControllersSID = "SID=" + $DomainControllersSID
 $descriptor = @($DomainComputersSID, $DomainControllersSID) -join " OR "
 
-Import-Module $PSScriptRoot\AzureArcDeployment.psm1
 $encryptedSecret = [DpapiNgUtil]::ProtectBase64($descriptor, $ServicePrincipalSecret)
+$encryptedSecret | Out-File -FilePath $SecretFile -Force
